81ed2b90bdd457bbed07c616016203ff5fff6c3c
Naim Katiman <naimkatiman@gmail.com>
2026-02-15 07:38:45 +0800

    chore: Remove obsolete PDRM e-Reporting HTML snapshot from output directory
    Delete static HTML snapshot of PDRM e-Reporting portal login page (1527 lines). File contained legacy ASP.NET WebForms markup with Telerik controls, ViewState, and hardcoded Bahasa Melayu/English UI elements. No longer needed in repository.

--- output/semakmule-config.js
+++
@@ -1,7 +0,0 @@
-var apiEndPoint = "https://mule.the-oaks.my/api/p/";
-var apiEndPointMule = "https://semakmule.rmp.gov.my/api/mule/";
-var latestStatsInterval = 30; // in seconds
-
-var disableCaptcha = true; 
-
-var maintenanceMode = false;// Set to true to enable maintenance mode
